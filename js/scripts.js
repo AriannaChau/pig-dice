d7b787d194ff036d4de879b251d0718acd0465a0
Arianna Chau and Steven Galvin <arianna@>
2017-03-27 20:26:35 -0700

    add js and html to display player names, hide new player submit button, and display a roll button instead. add code to generate new number every time roll button is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,30 @@ function Die() {
   this.sides = [1,2,3,4,5,6];
 }
 
-Player.prototype.hold = function() {
-
+Player.prototype.hold = function(turnScore, playerScore, turnCount) {
+  playerScore += turnScore;
+  turnCount++;
 }
 
 Die.prototype.roll = function(turnCount, turnScore) {
-var rolledSide = return this.sides[Math.floor(Math.random() * this.sides.length)];
+  var rolledSide = this.sides[Math.floor(Math.random() * this.sides.length)];
+  console.log(rolledSide)
   if (rolledSide === 1) {
     turnScore = 0
     turnCount++;
   } else {
-
+    turnScore += rolledSide;
   }
 }
 
+
+var whichTurn = function(player1, player2, turnCount) {
+  if (turnCount % 2 === 0) {
+    return player2;
+  } else {
+    return player1;
+  }
+}
 
 // front end logic
 
@@ -34,8 +44,23 @@ $(document).ready(function() {
     var player1 = $("input#player1").val();
     var player2 = $("input#player2").val();
 
-    var newPlayer1 = new Player(player1)
-    var newPlayer2 = new Player(player2)
+    newPlayer1 = new Player(player1)
+    newPlayer2 = new Player(player2)
+
+
+    $("button#submit").hide();
+    $("#rollBtn").show();
+    $("#show-players").show();
+    $("#playerName1").text(newPlayer1.playerName);
+    $("#playerName2").text(newPlayer2.playerName);
+    $("#turnScore1").text(newPlayer1.turnScore)
+    $("#turnScore2").text(newPlayer2.turnScore)
+  });
+
+  $("#rollBtn").click(function() {
+    var turnPlayer = whichTurn(player1, player2, turnCount);
+    die.roll(turnCount, turnPlayer.turnScore);
+    console.log(turnPlayer)
   });
 
 });
