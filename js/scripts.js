7e7c6dd9cc44a20fd621107f5b6bcf3c6aa63ec2
Arianna Chau and Steven Galvin <arianna@>
2017-03-27 19:35:35 -0700

    add die constructor, add roll prototype for die to generate random number. begin setup to roll die for specific player.

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,37 @@ function Player (name, playerScore) {
   this.turnScore = 0;
 }
 
+function Die() {
+  this.sides = [1,2,3,4,5,6];
+}
+
+Player.prototype.hold = function() {
+
+}
+
+Die.prototype.roll = function(turnCount, turnScore) {
+var rolledSide = return this.sides[Math.floor(Math.random() * this.sides.length)];
+  if (rolledSide === 1) {
+    turnScore = 0
+    turnCount++;
+  } else {
+
+  }
+}
+
+
 // front end logic
 
 $(document).ready(function() {
-  $("form#newPlayers").submit(function(event) {
+  $("form").submit(function(event) {
     event.preventDefault();
-
+    var die = new Die();
+    console.log(die.roll());
     var player1 = $("input#player1").val();
     var player2 = $("input#player2").val();
 
     var newPlayer1 = new Player(player1)
     var newPlayer2 = new Player(player2)
   });
+
 });
